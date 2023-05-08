8d405888aa481161f28bb66410426a7bd35e989d
Tobi <76131623+tobiglaser@users.noreply.github.com>
2023-05-08 21:34:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Raspberry Pi Pico FreeRTOS Project Template
+
+### Initialization:
+    git submodule update --init
+
+### Serial Terminal:
+    minicom -b 115200 -D /dev/ttyACM0
---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+
+
+void vTaskCode( void * pvParameters )
+{
+    /* The parameter value is expected to be 1 as 1 is passed in the
+    pvParameters value in the call to xTaskCreate() below. 
+    configASSERT( ( ( uint32_t ) pvParameters ) == 1 );
+    */
+    stdio_init_all();
+    const int LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    for( ;; )
+    {
+        printf("ON\n");
+        gpio_put(LED_PIN, 1);
+        vTaskDelay(500);
+        printf("OFF\n");
+        gpio_put(LED_PIN, 0);
+        vTaskDelay(500);
+    }
+}
+
+int main() {
+
+
+BaseType_t xReturned;
+TaskHandle_t xHandle = NULL;
+/* Create the task, storing the handle. */
+    xReturned = xTaskCreate(
+                    vTaskCode,       /* Function that implements the task. */
+                    "Blinky task",   /* Text name for the task. */
+                    512,             /* Stack size in words, not bytes. */
+                    ( void * ) 1,    /* Parameter passed into the task. */
+                    tskIDLE_PRIORITY,/* Priority at which the task is created. */
+                    &xHandle );   
+
+    vTaskStartScheduler();
+    while(1)
+    {
+        configASSERT(0);    /* We should never get here */
+    }
+
+}
